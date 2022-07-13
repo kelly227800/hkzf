55e7f0d8c734a8fa21dbc70f8f43546acd934130
kelly227800 <2529303733@qq.com>
2022-07-13 20:22:29 +0800

    完成了项目初始化

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+Vue.use(Vant)
 
 Vue.use(VueRouter)
 
-const routes = [
-
-]
+const routes = []
 
 const router = new VueRouter({
   routes
---
+++ src/utils/request.js
@@ -0,0 +1,10 @@
+/**
+ *
+ */
+import axios from 'axios'
+
+const request = axios.create({
+  baseURL: 'http://liufusong.top:8080' // 基础路径
+})
+
+export default request
